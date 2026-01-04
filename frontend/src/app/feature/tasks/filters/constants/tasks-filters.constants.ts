5e23473bfaf6c2d33117864976d24419b8f03d1f
NataliaJeszke <nataliajeszke8@gmail.com>
2026-01-04 11:21:05 +0100

    refactor types and constants

--- frontend/src/app/feature/tasks/filters/constants/tasks-filters.constants.ts
+++ frontend/src/app/feature/tasks/filters/constants/tasks-filters.constants.ts
@@ -1,3 +1,4 @@
+import { FilterType } from '@common/components/filters/constants/filters.constants';
 import { FilterConfig, FilterOption } from '@common/components/filters/model/filters.model';
 
 import { TASK_PRIORITIES, TASK_STATUSES } from '@feature/tasks/constants/tasks.constants';
@@ -12,39 +13,47 @@ export const TASK_PRIORITY_OPTIONS: readonly FilterOption[] = TASK_PRIORITIES.ma
   value: priority,
 }));
 
+export const TASK_KEYS = {
+  STATUS: 'status',
+  PRIORITY: 'priority',
+  AUTHOR_ID: 'authorId',
+  ASSIGNEE_ID: 'assigneeId',
+  CREATED_AT: 'createdAt',
+} as const;
+
 export const TASK_FILTERS_CONFIG: readonly FilterConfig[] = [
     {
-      key: 'status',
-      type: 'select',
+      key: TASK_KEYS.STATUS,
+      type: FilterType.SELECT,
       placeholder: 'features.tasks.filters.labels.status',
       options: TASK_STATUS_OPTIONS,
       width: '10rem',
     },
     {
-      key: 'priority',
-      type: 'select',
+      key: TASK_KEYS.PRIORITY,
+      type: FilterType.SELECT,
       placeholder: 'features.tasks.filters.labels.priority',
       options: TASK_PRIORITY_OPTIONS,
       width: '10rem',
     },
     {
-      key: 'authorId',
-      type: 'select',
+      key: TASK_KEYS.AUTHOR_ID,
+      type: FilterType.SELECT,
       placeholder: 'features.tasks.filters.labels.author',
       options: [],
       width: '18rem',
     },
     {
-      key: 'assigneeId',
-      type: 'select',
+      key: TASK_KEYS.ASSIGNEE_ID,
+      type: FilterType.SELECT,
       placeholder: 'features.tasks.filters.labels.assignee',
       options: [],
       width: '18rem',
     },
     {
-      key: 'createdAt',
-      type: 'date',
+      key: TASK_KEYS.CREATED_AT,
+      type: FilterType.DATE,
       placeholder: 'features.tasks.filters.labels.createdAt',
       width: '300px',
     },
-  ];
+  ] as const satisfies readonly FilterConfig[];
